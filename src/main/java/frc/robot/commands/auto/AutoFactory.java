ac585494531ba78d86a788e18bcb934c8a127504
TJ Vasquez <vasquezt.24@stu.materdei.org>
2023-03-08 13:58:21 -0800

    Corrected Auto Factory, added another switch

--- src/main/java/frc/robot/commands/auto/AutoFactory.java
+++ src/main/java/frc/robot/commands/auto/AutoFactory.java
@@ -35,46 +35,52 @@ public class AutoFactory extends SequentialCommandGroup {
 
         // STEP 2: Balance or move out of community
         
-        if (location == "Center") {
-            // We're located in the center so balance on the charge station
-            addCommands(new BalanceChargeStation(BotSubsystems.swerveDriver, isSideways));
-        } 
-        else if (location == "Left" || location == "Right") {
-            // We're located on the right or left so back out of community area
+        switch (location){
+            case "Center":
+                // We're located in the center so balance on the charge station
+                addCommands(new BalanceChargeStation(BotSubsystems.swerveDriver, isSideways));
+                break;
+        
+            //This is relative to the driver facing the field
+            case "Left":
+            case "Right":
+                // We're located on the right or left so back out of community area
 
-            double backingSpeed = 0.3;
-            // The side to side direction is opposite for Left and Right placements
-            double sideToSideSpeed = (location == "Left") ? 3.0 : -3.0;
+                double backingSpeed = 0.3;
+                // The side to side direction is opposite for Left and Right placements
+                double sideToSideSpeed = (location == "Left") ? -0.3 : 0.3;
 
-            double x1Speed, y1Speed, time1;
-            double x2Speed, y2Speed, time2;
-            if (isSideways) {
-                // The robot is initially positioned sideways, pointing toward the right
-                x1Speed = sideToSideSpeed;
-                y1Speed = 0.;
-                time1 = .4;
-                x2Speed = 0.;
-                y2Speed = backingSpeed;
-                time2 = 4.0;
+                double x1Speed, y1Speed, time1;
+                double x2Speed, y2Speed, time2;
+                if (isSideways) {
+                    // The robot is initially positioned sideways, pointing toward the right
+                    x1Speed = sideToSideSpeed;
+                    y1Speed = 0.;
+                    time1 = .4;
+                    x2Speed = 0.;
+                    y2Speed = backingSpeed;
+                    time2 = 4.0;
+                
+                }   else {
+                    // The robot is initially positioned toward the wall
+                    x1Speed = 0.;
+                    y1Speed = sideToSideSpeed;
+                    time1 = .4;
+                    x2Speed = -backingSpeed;
+                    y2Speed = 0.;
+                    time2 = 4.0;
+                }
+
+                //Move to the side of the field to clear the charge station
+                addCommands(new TimedSwerve(BotSubsystems.swerveDriver, x1Speed, y1Speed, 0., time1));
+                
+                //Drive outside the community
+                addCommands(new TimedSwerve(BotSubsystems.swerveDriver, x2Speed, y2Speed, 0., time2));
+
+                break;
             
-            }   else {
-                // The robot is initially positioned toward the wall
-                x1Speed = 0.;
-                y1Speed = sideToSideSpeed;
-                time1 = .4;
-                x2Speed = -backingSpeed;
-                y2Speed = 0.;
-                time2 = 4.0;
-            }
-
-            //Move to the side of the field to clear the charge station
-            addCommands(new TimedSwerve(BotSubsystems.swerveDriver, x1Speed, y1Speed, 0., time1));
-            
-            //Drive outside the community
-            addCommands(new TimedSwerve(BotSubsystems.swerveDriver, x2Speed, y2Speed, 0., time2));
-        }
-        else {
-            throw new java.lang.Error("Unkown Location; should be {Left, Center, Right}");           
+            default:
+                throw new java.lang.Error("Unkown Location; should be {Left, Center, Right}");           
         }
 
     }
